6aba1505cfdae474e4eb7c15f0fcfe5de0a89723
Oleg Ginzburg <olevole@srv-02.olevole.ru>
2022-01-18 16:21:59 +0300

    wip

--- scripts/pv-create-daemon.sh
+++ scripts/pv-create-daemon.sh
@@ -7,12 +7,12 @@
 #       -e ${PV_SPEC_SERVER} \
 #       -f "${INIT_MASTERS_IPS} ${INIT_NODES_IPS}" -g ${PV_SPEC_NFS_PATH} -h ${ZFS_K8S_PV_ROOT_MNT}
 
-while getopts "a:b:c:d:e:f:g:h" opt; do
+while getopts "a:b:c:d:e:f:g:h:" opt; do
 	case "${opt}" in
 		a) ZFS_K8S_PV_CLUSTER="${OPTARG}" ;;
 		b) ZFS_K8S_PV_CLUSTER_MNT="${OPTARG}" ;;
 		c) PV_SPEC_CAPACITY_STORAGE="${OPTARG}" ;;
-		d) LOGFILE="${OPTARG}" ;;
+		d) LOG_FILE="${OPTARG}" ;;
 		e) PV_SPEC_SERVER="${OPTARG}" ;;
 		f) IPS="${OPTARG}" ;;
 		g) PV_SPEC_NFS_PATH="${OPTARG}" ;;
@@ -22,11 +22,12 @@ while getopts "a:b:c:d:e:f:g:h" opt; do
 done
 
 if [ -z "${LOG_FILE}" ]; then
-	LOGFILE="/dev/stdout"
+	LOG_FILE="/dev/stdout"
 fi
 
+#echo "/usr/local/cbsd/modules/k8s.d/scripts/pv-create.sh -d \"${ZFS_K8S_PV_CLUSTER}\" -p \"${ZFS_K8S_PV_CLUSTER_MNT}\" -z \"${PV_SPEC_CAPACITY_STORAGE}\" -l ${LOG_FILE}"
 /usr/local/cbsd/modules/k8s.d/scripts/pv-create.sh -d "${ZFS_K8S_PV_CLUSTER}" -p "${ZFS_K8S_PV_CLUSTER_MNT}" -z "${PV_SPEC_CAPACITY_STORAGE}" -l ${LOG_FILE}
 /usr/local/cbsd/modules/k8s.d/scripts/nfs-enable.sh -h ${PV_SPEC_SERVER}
-/usr/local/cbsd/modules/k8s.d/scripts/exports-modify.sh -h "${INIT_MASTERS_IPS} ${INIT_NODES_IPS}" -p ${PV_SPEC_NFS_PATH} -r ${ZFS_K8S_PV_ROOT_MNT}
+/usr/local/cbsd/modules/k8s.d/scripts/exports-modify.sh -h "${IPS}" -p ${PV_SPEC_NFS_PATH} -r ${ZFS_K8S_PV_ROOT_MNT}
 
 exit 0
